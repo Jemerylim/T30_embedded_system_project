776cafafee37e59f2cc34b991e723210095fe80d
“Jeremy <limjeremy16@gmail.com>
2023-11-21 11:07:41 +0800

    barcode

--- irline/barcode_scanner.c
+++ irline/barcode_scanner.c
@@ -7,9 +7,10 @@
 #include "pico/time.h"
 #include <string.h>
 
-#define DIGITAL_PIN 22
+#define BARCODE_SENSOR 22
 #define ARRAY_SIZE 10
 #define DICTIONARY_SIZE 30
+#define DEBOUNCE 100
 
 struct bar{
     uint16_t voltage;
@@ -17,12 +18,14 @@ struct bar{
     int64_t barLength;
     int color; // black: 1 or white: 0
 };
-struct bar barList[ARRAY_SIZE];
+
+struct bar barList[ARRAY_SIZE]= {0};
 
 struct conversion {
     char key;
     char value[ARRAY_SIZE];
 };
+
 struct conversion dictionary[DICTIONARY_SIZE] = {
     {'A',"031312130"},
     {'B',"130312130"},
@@ -55,6 +58,9 @@ struct conversion dictionary[DICTIONARY_SIZE] = {
 
 int barcode[3][9];
 char decodedBarcode[3];
+volatile uint64_t interrupttime = 0;
+int barcodeIndex = 0;
+
 
 /*
 thickness classification
@@ -64,44 +70,44 @@ thick white 2
 thin white 3
 */
 
-
-static int isBlackOrWhite(uint32_t result){
-    int threshold = 800; //more than 800 is black
-    if (result >= threshold || gpio_get(DIGITAL_PIN) == 1) { //black
-        return 1;
-    }else{
-        return 0;
-    }
+void init_pins() {
+    gpio_init(BARCODE_SENSOR);
+    gpio_set_dir(BARCODE_SENSOR, GPIO_IN);
 }
 
 static int handleBars(uint32_t result, int index){
-    int blackOrWhite = isBlackOrWhite(result); //get color
     absolute_time_t current_time = get_absolute_time(); //get start time
     if(index == 0){
-        if(blackOrWhite == 1){ //only add if its black
-            barList[index].voltage = result;
-            barList[index].barLength = -1;
-            barList[index].color = blackOrWhite;
-            barList[index].bar_start = current_time; //set start time
-            return 1;
-        }else{// barcode has not start
-            return 0;
-        }
+        //only add if its black
+        barList[index].voltage = result;
+        barList[index].barLength = -1;
+        barList[index].color = 1;
+        barList[index].bar_start = current_time; //set start time
+        printf("Bar %d - Voltage: %d, Color: %s, Start: %llu, len:  %llu\n" , index, barList[index].voltage, barList[index].color == 1 ? "Black" : "White", barList[index].bar_start, barList[index].barLength); 
+        return 1;
     }else{
-        if(barList[index-1].color != blackOrWhite){// if bar is not the same color as the last one saved
-            if(index == 1 || index == 9){
-                int64_t length =  absolute_time_diff_us(barList[index-1].bar_start,current_time);
-                barList[index-1].barLength = length - 20000; //adjusted for some anomaly values
+        if(absolute_time_diff_us(barList[index-1].bar_start,current_time)>1000){
+            if (barList[index-1].color == 1){
+                //white
+                barList[index-1].barLength = absolute_time_diff_us(barList[index-1].bar_start,current_time);
+                barList[index].voltage = result;
+                barList[index].barLength = -1;
+                barList[index].color = 0;
+                barList[index].bar_start = current_time;
+                printf("Bar %d - Voltage: %d, Color: %s, Start: %llu, len:  %llu\n" , index-1, barList[index-1].voltage, barList[index-1].color == 1 ? "Black" : "White", barList[index-1].bar_start, barList[index-1].barLength); 
+                return 0;
             }else{
-                barList[index-1].barLength = absolute_time_diff_us(barList[index-1].bar_start,current_time); //minus current time with last bar start time to get length
+                //black
+                barList[index-1].barLength = absolute_time_diff_us(barList[index-1].bar_start,current_time);
+                barList[index].voltage = result;
+                barList[index].barLength = -1;
+                barList[index].color = 1;
+                barList[index].bar_start = current_time;
+                printf("Bar %d - Voltage: %d, Color: %s, Start: %llu, len:  %llu\n" , index-1, barList[index-1].voltage, barList[index-1].color == 1 ? "Black" : "White", barList[index-1].bar_start, barList[index-1].barLength); 
+                return 1;
             }
-            barList[index].voltage = result;
-            barList[index].barLength = -1;
-            barList[index].color = blackOrWhite;
-            barList[index].bar_start = current_time;
-            return index + 1;
         }else{
-            return index;
+            return 0;
         }
     }
 }
@@ -126,8 +132,7 @@ static void classifyThickness(int barcodeIndex){
             }else{//white
                 barcode[barcodeIndex][i] = 3;
             }
-        }
-        //printf("%d",barcode[barcodeIndex][i]);    
+        } 
     }
 }
 static int decodeBarCode(int barcodeIndex){
@@ -163,51 +168,54 @@ static int decodeBarCode(int barcodeIndex){
 
 static void readBarcode(){
     if(decodedBarcode[0] == '*' && decodedBarcode[2] =='*' ){
-        printf("%c %c %c",decodedBarcode[0],decodedBarcode[1]decodedBarcode[2]);
+        printf("%c %c %c",decodedBarcode[0],decodedBarcode[1],decodedBarcode[2]);
         printf("Barcode says %c",decodedBarcode[1]);
     }else{
-        printf("%c %c %c",decodedBarcode[0],decodedBarcode[1]decodedBarcode[2]);
+        printf("%c %c %c",decodedBarcode[0],decodedBarcode[1],decodedBarcode[2]);
         printf("Barcode is not valid because the first and last values are not *");
     }
 }
 
+void interrupt_callback(uint gpio, uint32_t events) {
+    uint64_t current = to_ms_since_boot(get_absolute_time());
+    if ((current - interrupttime) < DEBOUNCE)
+    {
+        return;
+    }
+    interrupttime = current;
+    if(gpio==BARCODE_SENSOR){
+        adc_select_input(0);
+        uint32_t result = adc_read(); //result is a range between 0 - ~2000, above 850 is black
+        int index = -1;
+        for (int i = 0; i < ARRAY_SIZE; i++) {
+            if (barList[i].voltage==0)
+            {
+                index = i;
+                break;
+            }        
+        }
+        if(index == -1 ){
+            //all 9 slots are full
+            if(decodeBarCode(barcodeIndex)==1){
+                barcodeIndex+=1;
+            }
+            for (int i = 0; i < ARRAY_SIZE; i++) {
+                barList[i].voltage = 0;
+            }
+        }else{
+            handleBars(result,index);
+        }
+        
+    }
+
+}
+
 int main() {
     stdio_usb_init();
+    init_pins();
     adc_init();
-    adc_select_input(0);
-    int barcodeIndex =0;
-    int index = 0;
-    while (1) {
-        uint32_t result = adc_read(); //result is a range between 0 - ~2000, above 850 is black
-        // adc_select_input(1); // ADC1 GP27
-        // uint32_t result_left = adc_read(); // voltage above 1 should be black
-        // const float conversion_factor = 3.3f / (1 << 12);
-        // printf("\n0x%03x -> %f V\n", result_left, result_left * conversion_factor);
-        // adc_select_input(2); // ADC2 GP28
-        // uint32_t result_right = adc_read();
-        // printf("\n0x%03x -> %f V\n", result_right, result_right * conversion_factor);
-        index = handleBars(result,index);
-        for (int i = 0; i < 10; i++) {
-            printf("Bar %d - Voltage: %d, Color: %s, Start: %llu, len:  %llu\n" , i + 1, barList[i].voltage, barList[i].color == 1 ? "Black" : "White", barList[i].bar_start, barList[i].barLength);
+    gpio_set_irq_enabled_with_callback(BARCODE_SENSOR, GPIO_IRQ_EDGE_RISE, true, &interrupt_callback);
+    while (true) {
+            tight_loop_contents();
         }
-       if(index>9){
-            if(decodeBarCode(barcodeIndex)==0){
-                index =0;
-                //sleep_ms(3000);
-            }else{
-                if(barcodeIndex < 3){
-                    index =0;
-                    printf("%c",decodedBarcode[barcodeIndex]);
-                    barcodeIndex+=1;
-                    //sleep_ms(3000);
-                }else{
-                    printf("done");
-                    readBarcode();
-                    index =0;
-                    barcodeIndex=0;
-                    sleep_ms(3000);
-                }
-            }
-        }
-    }
 }
